d07736eab796e5e48df5d2957fe959428c59c1ca
Aux <justin@akitaux.com>
2023-04-27 03:53:53 -0500

    Fix auto terminate process

--- src/WebcomicNotify/Commands/RootCommand.cs
+++ src/WebcomicNotify/Commands/RootCommand.cs
@@ -1,5 +1,13 @@
-﻿using System.CommandLine;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.CommandLine;
+using System.CommandLine.Help;
+using System.CommandLine.NamingConventionBinder;
+using System.Runtime.CompilerServices;
 using WebcomicNotify.Commands;
+using WebcomicNotify.Services;
 
 namespace WebcomicNotify
 {
@@ -19,6 +27,8 @@ namespace WebcomicNotify
         public RootCommand()
             : base("A cli app for polling and notifying of when new webcomic chapters are released.")
         {
+            Aliases.Add("run");
+            Aliases.Add("start");
             Options.Add(DefaultWebhook);
             Options.Add(PollRateMinutes);
 
@@ -26,6 +36,35 @@ namespace WebcomicNotify
             Subcommands.Add(new CheckCommand());
             Subcommands.Add(new ListCommand());
             Subcommands.Add(new RemoveCommand());
+
+            Action = CommandHandler.Create<ParseResult, IHost>(ExecuteAsync);
+        }
+
+        public async Task<int> ExecuteAsync(ParseResult result, IHost host)
+        {
+            if (result.Tokens.Any(x => new[] {"--help", "-h", "/h", "-?", "/?" }.Contains(x.Value)))
+                return 2;
+
+            var logger = host.Services.GetRequiredService<ILogger<RootCommand>>();
+            var url = result.GetValue(DefaultWebhook);
+            if (!Uri.TryCreate(url, UriKind.Absolute, out var defaultWebhookUrl))
+            {
+                logger.LogError("The value `{webhook}` is not a valid url.", defaultWebhookUrl);
+                return 1;
+            }
+
+            var pollRateMinutes = result.GetValue(PollRateMinutes);
+            if (pollRateMinutes < 1)
+            {
+                logger.LogError("The value `{pollrate}` is not a valid poll rate", pollRateMinutes);
+                return 1;
+            }
+
+            logger.LogInformation("Running service with default webhook {webhook} and a polling rate of {pollrate} minute(s)", defaultWebhookUrl, pollRateMinutes);
+
+            await host.RunAsync();
+            host.Dispose();
+            return 0;
         }
     }
 }
--- src/WebcomicNotify/Program.cs
+++ src/WebcomicNotify/Program.cs
@@ -7,24 +7,31 @@ using WebcomicNotify;
 using WebcomicNotify.Services;
 
 var hostBuilder = Host.CreateDefaultBuilder()
+    .ConfigureLogging(options =>
+    {
+        options.AddSimpleConsole(options => options.SingleLine = true);
+    })
     .ConfigureServices(services =>
     {
         services.AddSingleton<DataService>();
+        services.AddTransient<FeedFinderService>();
     });
 
-if (args.Length != 0)
+var cli = new CliConfiguration(new RootCommand())
 {
-    var cli = new CliConfiguration(new RootCommand())
-        .UseHost(_ => hostBuilder.ConfigureLogging(options =>
-        {
-            options.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.None);
-        }));
+    ProcessTerminationTimeout = null
+}.UseHost(_ => hostBuilder);
 
-    await cli.InvokeAsync(args);
-} 
+// There's no way to disable cli.ProcessTerminationTimeout so we
+// have to do this mess to run the host service.
+var result = cli.Parse(args);
+
+if (result.CommandResult.Command is not RootCommand root)
+    await result.InvokeAsync();
 else
 {
-    await hostBuilder
-        .Build()
-        .RunAsync();
+    // Executing the root command directly disables help, so we
+    // return 2 if it detects the help command arguments
+    int r = await root.ExecuteAsync(result, hostBuilder.Build());
+    if (r == 2) cli.Invoke(args);
 }
